bc40da35c3b0b8d9c590b857db4cb88da66cfb66
changmin1996 <changmin@wego-robotics.com>
2024-02-08 14:47:00 +0900

    start to change camera application

--- limo_ros2/limo_ros2_application/limo_ros2_application/detect_line.py
+++ limo_ros2/limo_ros2_application/limo_ros2_application/detect_line.py
@@ -8,54 +8,76 @@ import numpy as np
 
 class DetectLine(Node):
     def __init__(self):
-        super().__init__('detect_line')
-        self.subscription = self.create_subscription(Image, '/camera/color/image_raw', self.image_callback, rclpy.qos.qos_profile_sensor_data)
-        self.subscription
+        # CV bridge
         self.br = CvBridge()
-        self.roi_publisher = self.create_publisher(Image, 'roi_image', 10)
-        self.mask_publisher = self.create_publisher(Image, 'mask_image', 10)
-        self.debug_publisher = self.create_publisher(Image, 'debug_image', 10)
+
+        # Subscribe camera data
+        super().__init__('detect_line')
+        self.subscription = self.create_subscription(
+                            Image,
+                            '/camera/color/image_raw', 
+                            self.image_callback, 
+                            rclpy.qos.qos_profile_sensor_data)
+        self.subscription # to prevent from warning
+
+        # Publish result (offset between reference distance and real distance)
         self.dis_publisher = self.create_publisher(Int32, 'distance_y', 10)
 
+        # Publish Image for debugging
+        self.debug_publisher = self.create_publisher(Image, 'debug_image', 10)
+        self.timer_ = self.create_timer(0.1, self.timer_callback)
+
+        # Parameters (For Masking Lane, For the reference distance of lane, ROI)
         self.yellow_lane_low = np.array([0, 90, 100])
         self.yellow_lane_high = np.array([60, 220, 255])
-        
 
+        # Parameter For debugging sequence
+        # 0: ROI
+        # 1: Masking
+        # 2: Moment of lane
+        self.debug_sequence = 2
+    
+    def timer_callback(self):
+        if self.debug_sequence == 0:
+            self.debug_publisher.publish(self.br.cv2_to_imgmsg(self.roi_,'bgr8'))
+        elif self.debug_sequence == 1:
+            self.debug_publisher.publish(self.br.cv2_to_imgmsg(self.mask_yellow,'mono8'))
+        else:    
+            self.debug_publisher.publish(self.br.cv2_to_imgmsg(self.image_,'bgr8'))
+          
     def image_callback(self, msg):
-        image_ = self.br.imgmsg_to_cv2(msg, 'bgr8')
+        # convert opencv Mat type to image msg type
+        self.image_ = self.br.imgmsg_to_cv2(msg, 'bgr8')
         
-        roi_ = image_[350:400, 0:320]
+        # Make Region of Interest
+        self.roi_ = self.image_[350:400, 0:320]
+
+        # Masking the lane
         hls = cv2.cvtColor(roi_, cv2.COLOR_BGR2HLS)
-        mask_yellow = cv2.inRange(hls, self.yellow_lane_low, self.yellow_lane_high)
+        self.mask_yellow = cv2.inRange(hls, self.yellow_lane_low, self.yellow_lane_high)
         
-        M = cv2.moments(mask_yellow)
+        # Calculating the Moment of the lane
+        M = cv2.moments(self.mask_yellow)
         if M['m00'] > 0:
             cx = int(M['m10']/M['m00'])
             cy = int(M['m01']/M['m00'])
             cy = 350 + cy
-            image_ = cv2.circle(image_, (cx, cy), 10,(255, 0, 0), -1)
+            self.image_ = cv2.circle(self.image_, (cx, cy), 10,(255, 0, 0), -1)
             distance_to_ref = 250 -cx
-        else:
+        else: # When limo cannot find lane publish garbage data
             distance_to_ref = -999
 
+        # Publishing the offset between current distance with lane and reference distance
         dis = Int32()
         dis.data = distance_to_ref
-        
         self.dis_publisher.publish(dis)
-        self.roi_publisher.publish(self.br.cv2_to_imgmsg(roi_,'bgr8'))
-        self.debug_publisher.publish(self.br.cv2_to_imgmsg(image_,'bgr8'))
-        self.mask_publisher.publish(self.br.cv2_to_imgmsg(mask_yellow,'mono8'))
 
 def main(args=None):
     rclpy.init(args=args)
 
     detect_line = DetectLine()
-
     rclpy.spin(detect_line)
 
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
     detect_line.destroy_node()
     rclpy.shutdown()
 
